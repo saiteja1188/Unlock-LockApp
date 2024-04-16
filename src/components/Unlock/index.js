9a8a8eff78fb9a2e8d0c9fe8c300c01f2c2143b8
saiteja1188 <loveui19@gmail.com>
2024-04-16 17:02:41 +0000

    Cd Unlock/lock app

--- src/components/Unlock/index.js
+++ src/components/Unlock/index.js
@@ -1,48 +1,34 @@
-// Write your code here
 import {useState} from 'react'
 
 import {
-  AppContainer,
-  LockContainer,
-  LockImage,
-  Paragraph,
+  UnlockLockContainer,
+  ImageAndTextContainer,
+  Image,
+  Text,
   Button,
 } from './styledComponents'
 
 const Unlock = () => {
-  const [isActive, setIsActive] = useState(false)
-  const onClickButton = () => setIsActive(Prev => !Prev)
-
-  const imageUrl = isActive
-    ? 'https://assets.ccbp.in/frontend/hooks/unlock-img.png '
+  const [isUnlocked, setIsUnlocked] = useState(false)
+  const imageUrl = isUnlocked
+    ? 'https://assets.ccbp.in/frontend/hooks/unlock-img.png'
     : 'https://assets.ccbp.in/frontend/hooks/lock-img.png'
+  const altText = isUnlocked ? 'unlock' : 'lock'
+  const text = isUnlocked ? 'Your Device is Unlocked' : 'Your Device is Locked'
+  const buttonText = isUnlocked ? 'Lock' : 'Unlock'
 
-  const paragraphText = isActive
-    ? 'Your Device is Unlocked'
-    : 'Your Device is Locked'
-
-  const buttonText = isActive ? 'Lock' : 'Unlock'
+  const onClickButton = () => setIsUnlocked(prevStatus => !prevStatus)
 
   return (
-    <AppContainer>
-      {isActive ? (
-        <LockContainer>
-          <LockImage src={imageUrl} alt="lock" />
-          <Paragraph>{paragraphText}</Paragraph>
-          <Button type="button" onClick={onClickButton}>
-            {buttonText}
-          </Button>
-        </LockContainer>
-      ) : (
-        <LockContainer>
-          <LockImage src={imageUrl} alt="lock" />
-          <Paragraph>{paragraphText}</Paragraph>
-          <Button type="button" onClick={onClickButton}>
-            {buttonText}
-          </Button>
-        </LockContainer>
-      )}
-    </AppContainer>
+    <UnlockLockContainer>
+      <ImageAndTextContainer>
+        <Image src={imageUrl} alt={altText} />
+        <Text>{text}</Text>
+      </ImageAndTextContainer>
+      <Button type="button" onClick={onClickButton}>
+        {buttonText}
+      </Button>
+    </UnlockLockContainer>
   )
 }
 
--- src/components/Unlock/styledComponents.js
+++ src/components/Unlock/styledComponents.js
@@ -1,38 +1,53 @@
-// Style your elements here
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
 
-export const AppContainer = styled.div`
+export const UnlockLockContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-image: linear-gradient(to bottom, #161617, #3c2940);
-  height: 100vh;
+  background-image: linear-gradient(to bottom, #161617, #0b0c1e, #3c2940);
+  min-height: 100vh;
 `
-export const LockContainer = styled.div`
+
+export const ImageAndTextContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `
 
-export const LockImage = styled.img`
-  width: 100px;
-  height: 100px;
+export const Image = styled.img`
+  width: 96px;
 `
-export const Paragraph = styled.p`
-  font-family: Roboto;
-  font-size: 18px;
-  font-weight: 500;
-  color: #ffffff;
-  margin-top: 20px;
+export const Text = styled.p`
+  color: #e2e8f0;
+  font-family: 'Roboto';
+  font-size: 16px;
+  margin-top: 24px;
+  margin-bottom: 90px;
+  line-height: 1.2;
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+    margin-bottom: 68px;
+    line-height: 1.5;
+  }
 `
 export const Button = styled.button`
-  background-color: #06b6d4;
   color: #ffffff;
-  padding: 8px 18px;
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 16px;
+  background-color: #06b6d4;
   border: none;
-  outline: none;
+  border-radius: 12px;
+  margin-top: 90px;
+  padding-top: 12px;
+  padding-right: 24px;
+  padding-bottom: 12px;
+  padding-left: 24px;
   cursor: pointer;
-  border-radius: 10px;
-  margin-top: 100px;
+  outline: none;
+  line-height: 1.33;
+  @media screen and (min-width: 768px) {
+    margin-top: 68px;
+  }
 `
